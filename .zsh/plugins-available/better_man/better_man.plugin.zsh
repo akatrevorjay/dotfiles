4259dd9ebb915cd9c74b988540588d1b85e5e0af
Trevor Joynson (trevorj) <github@skywww.net>
2016-06-10 06:47:46 -0400

    Fix better_man

--- .zsh/plugins-available/better_man/better_man.plugin.zsh
+++ .zsh/plugins-available/better_man/better_man.plugin.zsh
@@ -1,43 +1,28 @@
 #!/bin/zsh
 
-if [[ "$OSTYPE" = solaris* ]]
-then
-	if [[ ! -x "$HOME/bin/nroff" ]]
-	then
-		mkdir -p "$HOME/bin"
-		cat > "$HOME/bin/nroff" <<-EOF
-		#!/bin/sh
-		if [ -n "\$_NROFF_U" -a "\$1,\$2,\$3" = "-u0,-Tlp,-man" ]; then
-			shift
-			exec /usr/bin/nroff -u\$_NROFF_U "\$@"
-		fi
-		#-- Some other invocation of nroff
-		exec /usr/bin/nroff "\$@"
-		EOF
-		chmod +x "$HOME/bin/nroff"
-	fi
-fi
+-colored-man() {
+	local width=$(tput cols)
+	[[ $width -gt $MANWIDTH ]] && width=$MANWIDTH
 
-__colored_man() {
 	env \
-		LESS_TERMCAP_mb=$(printf "\e[1;31m") \
-		LESS_TERMCAP_md=$(printf "\e[1;31m") \
-		LESS_TERMCAP_me=$(printf "\e[0m") \
-		LESS_TERMCAP_se=$(printf "\e[0m") \
-		LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
-		LESS_TERMCAP_ue=$(printf "\e[0m") \
-		LESS_TERMCAP_us=$(printf "\e[1;32m") \
-		PAGER="${commands[less]:-$PAGER}" \
-		_NROFF_U=1 \
-		PATH="$HOME/bin:$PATH" \
-		\man "$@"
+	  LESS_TERMCAP_mb=$(printf "\e[1;31m") \
+	  LESS_TERMCAP_md=$(printf "\e[1;31m") \
+	  LESS_TERMCAP_me=$(printf "\e[0m") \
+	  LESS_TERMCAP_se=$(printf "\e[0m") \
+	  LESS_TERMCAP_so=$(printf "\e[1;44;33m") \
+	  LESS_TERMCAP_ue=$(printf "\e[0m") \
+	  LESS_TERMCAP_us=$(printf "\e[1;32m") \
+	  PAGER="${commands[less]:-$PAGER}" \
+	  _NROFF_U=1 \
+	  MANWIDTH=$width \
+	  \man "$@"
 }
 
 if [[ "$CURRENT_SHELL" == "zsh" ]]; then
 	alias help=run-help
 fi
 
-__better_man () {
+-better-man () {
 	local cmdtype help
 	case "$CURRENT_SHELL" in
 		zsh)
@@ -61,14 +46,14 @@ __better_man () {
 			;;
 		# something else, presumed to be an external command or options for the man command or a section number
 		*)
-			__colored_man "$@"
+			-colored-man "$@"
 			;;
 	esac
 }
 
-#alias man=__better_man
+#alias man=-better-man
 
 man() {
-    __better_man "$@"
+    -better-man "$@"
 }
 
