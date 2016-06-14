f93f8fa9a93407e66d99e957b656285c16752a90
Trevor Joynson (trevorj) <github@skywww.net>
2016-06-13 22:18:42 -0400

    Fuck it: Autoload everything we can.

--- .zsh/plugins-available/completion/completion.plugin.zsh
+++ .zsh/plugins-available/completion/completion.plugin.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 # Maybe
-setopt completeinword completealiases
+setopt completeinword
 
 # completion
 zstyle ':completion:*' auto-description 'specify: %d'
@@ -9,9 +9,8 @@ zstyle ':completion:*' completer _expand _complete _correct _approximate
 zstyle ':completion:*' format 'Completing %d'
 zstyle ':completion:*' group-name ''
 zstyle ':completion:*' menu select=2
-eval "$(dircolors -b)"
 zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
-zstyle ':completion:*' list-colors ''
+#zstyle ':completion:*' list-colors ''
 zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
 zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
 zstyle ':completion:*' menu select=long
