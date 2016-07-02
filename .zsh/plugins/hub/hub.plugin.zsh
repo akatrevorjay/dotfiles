bdd6723f451ef62a7d3e4160df2a05b8c0328fe9
Trevor Joynson (trevorj) <github@skywww.net>
2016-07-02 17:45:36 -0400

    Fix hub completions when installed in GOPATH, this should really be integrated better into hub..

--- .zsh/plugins/hub/hub.plugin.zsh
+++ .zsh/plugins/hub/hub.plugin.zsh
@@ -1,6 +1,9 @@
 #!/bin/zsh
 if (( ${+commands[hub]} )); then
 	eval "$(hub alias -s)"
+
+	local completions=${_HUB_COMPLETIONS:-"$GOPATH/src/github.com/hub/etc/hub.completions.zsh"}
+	[[ ! -f "$completions" ]] || source "$completions"
 else
 	echo "Could not find 'hub' command. Not adding aliases." >&2
 fi
