349e40605561269a9375edb7187989c00fcee8d9
Trevor Joynson (trevorj) <github@skywww.net>
2016-07-02 03:28:58 -0400

    OSX ensure, allow gomi in condom

--- .zsh/plugins-extra/trevorj/trevorj.plugin.zsh
+++ .zsh/plugins-extra/trevorj/trevorj.plugin.zsh
@@ -4,5 +4,9 @@ export DEBEMAIL=debian@trevor.joynson.io
 
 # GPG 2.1.x SSH support
 # See : http://incenp.org/notes/2015/gnupg-for-ssh-authentication.html
-export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
+#export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
+
+# WHy CAn't I TYpe SHift?
+alias Grep='grep'
+alias Less='less'
 
--- .zsh/plugins/condom/condom.plugin.zsh
+++ .zsh/plugins/condom/condom.plugin.zsh
@@ -2,38 +2,28 @@
 
 #[[ "$UID" -gt 0 ]] || return
 
-if (( ${+commands[gomi]} )); then
-	:
-fi
-
 case "${OSTYPE:l}" in
 	# No condoms for you, you sly devil.
 	# (osx just doesn't support these opts reliably.)
-	darwin*) return ;;
+	darwin*)
+		alias chmod='chmod -v'
+		alias chown='chown -v'
+		# from gnubin
+		#! (( ${+commands[grm]} )) || alias rm='grm --preserve-root -vdI'
+		;;
 
 	linux*)
 		alias chmod='chmod --preserve-root --changes'
 		alias chown='chown --preserve-root --changes'
 		alias rm='rm --preserve-root -vdI'
-
-		# not aliasing rm -i, but if safe-rm is available, use condom.
-		if (( ${+commands[sm]} )) && (( ! ${+commands[sm]} )); then
-			alias rm='sm -vh'
-		elif (( ${+commands[safe-rm]} )) && (( ! ${+commands[safe-rmdir]} )); then
-			alias rm='safe-rm -v'
-		fi
 		;;
 esac
 
-#
-# Misc
-#
-
-mkcd() {
-    [[ -n ${1} ]] && mkdir -p ${1} && builtin cd ${1}
-}
-
-# WHy CAn't I TYpe SHift? :hate:
-alias Grep='grep'
-alias Less='less'
+if (( ${+commands[gomi]} )); then
+	alias rm='gomi'
+elif (( ${+commands[sm]} )); then
+	alias rm='sm -vh'
+elif (( ${+commands[safe-rm]} )) && (( ! ${+commands[safe-rmdir]} )); then
+	alias rm='safe-rm -v'
+fi
 
