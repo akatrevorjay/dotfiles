40af2d71538215fe105609d7a63e4d66497872a1
Trevor Joynson (trevorj) <github@skywww.net>
2016-07-03 02:29:16 -0400

    Finish

--- .zsh/plugins-extra/trevorj/trevorj.plugin.zsh
+++ .zsh/plugins-extra/trevorj/trevorj.plugin.zsh
@@ -6,6 +6,9 @@ export DEBEMAIL=debian@trevor.joynson.io
 # See : http://incenp.org/notes/2015/gnupg-for-ssh-authentication.html
 #export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
 
+# Default docker machine
+#: ${DOCKER_MACHINE_NAME:="dev"}
+
 # WHy CAn't I TYpe SHift?
 alias Grep='grep'
 alias Less='less'
