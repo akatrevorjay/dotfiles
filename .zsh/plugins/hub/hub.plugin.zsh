3de3cedff569e7ebbf35dc9921fc7754411caba3
Trevor Joynson (trevorj) <github@skywww.net>
2016-07-03 03:02:02 -0400

    Betterify hub completions

--- .zsh/plugins/hub/hub.plugin.zsh
+++ .zsh/plugins/hub/hub.plugin.zsh
@@ -3,10 +3,14 @@ if (( ${+commands[hub]} )); then
 	eval "$(hub alias -s)"
 
 	: ${_HUB_COMPLETIONS:="$GOPATH/src/github.com/github/hub/etc/hub.zsh_completion"}
-	if [[ -e "$_HUB_COMPLETIONS" ]]; then
-		source "$_HUB_COMPLETIONS"
-	else
-		echo "Could not find 'hub' completions at _HUB_COMPLETIONS=$_HUB_COMPLETIONS" >&2
+	if [[ ! -e "$LOCAL_ZDOTDIR/completions/_hub" ]]; then
+		if [[ -e "$_HUB_COMPLETIONS" ]]; then
+			echo "Creating symlink for hub completions" >&2
+			mkdir -pv $LOCAL_ZDOTDIR/completions
+			ln -sfv $_HUB_COMPLETIONS $LOCAL_ZDOTDIR/completions/_hub
+		else
+			echo "Could not find 'hub' completions at _HUB_COMPLETIONS=$_HUB_COMPLETIONS" >&2
+		fi
 	fi
 else
 	echo "Could not find 'hub' command. Not adding aliases." >&2
