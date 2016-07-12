685f1718c650214e274c3ce88172cd79d91c48d4
Trevor Joynson (trevorj) <github@skywww.net>
2016-07-12 04:32:29 -0400

    Cache thefuck as it's slow as balls

--- .config/zsh/plugins-extra/thefuck/thefuck.plugin.zsh
+++ .config/zsh/plugins-extra/thefuck/thefuck.plugin.zsh
@@ -2,5 +2,11 @@
 
 (( ${+commands[thefuck]} )) || return
 
-eval "$(thefuck --alias)"
+: ${_THEFUCK_INIT_CACHE:=${ZSH_CACHE_DIR:-${ZDOTDIR:-$HOME}}/.thefuck-init.zsh}
+
+if [ ${commands[thefuck]} -nt $_THEFUCK_INIT_CACHE -o ! -s $_THEFUCK_INIT_CACHE ]; then
+    thefuck --alias \
+      >| $_THEFUCK_INIT_CACHE
+fi
+source $_THEFUCK_INIT_CACHE
 
