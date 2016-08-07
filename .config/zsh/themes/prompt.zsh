89adc2cdfc100014c414b2c309377bde444d3937
Trevor Joynson (trevorj) <github@skywww.net>
2016-08-06 22:30:29 -0700

    Lame.

--- .config/zsh/themes/prompt.zsh
+++ .config/zsh/themes/prompt.zsh
@@ -1,2 +1,13 @@
 #!/bin/zsh
-source ${0:A}
+if (( ! $+functions[prompt] )); then
+    autoload -Uz promptinit
+    promptinit
+fi
+
+case $0:t in
+    prompt.zsh|${${0:A}:t}) ;;
+    *)
+        prompt ${${0:t}%.zsh}
+    ;;
+esac
+
