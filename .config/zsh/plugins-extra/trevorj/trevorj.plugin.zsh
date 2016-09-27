87e2a45c41ac46612ef085217235a9fc6afb08b3
Trevor Joynson (trevorj) <github@skywww.net>
2016-09-26 20:40:47 -0700

    Add EDITOR

--- .config/zsh/plugins-extra/trevorj/trevorj.plugin.zsh
+++ .config/zsh/plugins-extra/trevorj/trevorj.plugin.zsh
@@ -2,6 +2,11 @@
 
 export DEBEMAIL=debian@trevor.joynson.io
 
+(( ${+commands[nvim]} )) && : ${EDITOR:=nvim}
+(( ${+commands[vim]} )) && : ${EDITOR:=vim}
+(( ${+commands[vi]} )) && : ${EDITOR:=vi}
+export EDITOR
+
 # GPG 2.1.x SSH support
 # See : http://incenp.org/notes/2015/gnupg-for-ssh-authentication.html
 #export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
