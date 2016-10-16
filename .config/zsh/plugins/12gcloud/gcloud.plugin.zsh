b0eb4c867ba369c5b5329cf453c7d6cf5cdac14a
Trevor Joynson (trevorj) <github@skywww.net>
2016-10-16 04:13:57 -0700

    kubectl comp

--- .config/zsh/plugins/12gcloud/gcloud.plugin.zsh
+++ .config/zsh/plugins/12gcloud/gcloud.plugin.zsh
@@ -1,7 +1,17 @@
 #!/bin/zsh
 # Google Cloud SDK
 : ${GCLOUD_SDK_ROOT:="$HOME/.local/google-cloud-sdk"}
-[[ -e $GCLOUD_SDK_ROOT ]] || return 1
 
-source $GCLOUD_SDK_ROOT/path.zsh.inc
-source $GCLOUD_SDK_ROOT/completion.zsh.inc
+if [[ -e $GCLOUD_SDK_ROOT ]]; then
+    source $GCLOUD_SDK_ROOT/path.zsh.inc
+    source $GCLOUD_SDK_ROOT/completion.zsh.inc
+fi
+
+if [[ ${+commands[kubectl]} ]]; then
+    : ${_KUBECTL_INIT_CACHE:=${ZSH_CACHE_DIR:-${ZDOTDIR:-$HOME}}/.kubectl-init.zsh}
+    if [ ${commands[kubectl]} -nt $_KUBECTL_INIT_CACHE -o ! -s $_KUBECTL_INIT_CACHE ]; then
+        kubectl completion zsh \
+          >| $_KUBECTL_INIT_CACHE
+    fi
+    source $_KUBECTL_INIT_CACHE
+fi
