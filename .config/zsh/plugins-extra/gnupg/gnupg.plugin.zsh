f3ae3d377be2d4a50d7180d9e222c7c395586b6b
Trevor Joynson (trevorj) <github@skywww.net>
2016-10-28 14:53:51 -0700

    Fix gnupg

--- .config/zsh/plugins-extra/gnupg/gnupg.plugin.zsh
+++ .config/zsh/plugins-extra/gnupg/gnupg.plugin.zsh
@@ -1,18 +1,13 @@
 #!/bin/zsh
 
-## Start the gpg-agent if not already running
-#if ! pgrep -x -u "${USER}" gpg-agent >/dev/null 2>&1; then
-#  gpg-connect-agent /bye >/dev/null 2>&1
-#fi
-
 ## Set GPG TTY
-#export GPG_TTY=$(tty)
+export GPG_TTY=$(tty)
 
 ## Refresh gpg-agent tty in case user switches into an X session
-#gpg-connect-agent updatestartuptty /bye >/dev/null
+gpg-connect-agent updatestartuptty /bye >/dev/null
 
 # Set SSH to use gpg-agent
 unset SSH_AGENT_PID
 if [ "${gnupg_SSH_AUTH_SOCK_by:-0}" -ne $$ ]; then
-    export SSH_AUTH_SOCK="$HOME/.gnupg/S.gpg-agent.ssh"
+    export SSH_AUTH_SOCK="$(gpgconf --list-dirs agent-ssh-socket)"
 fi
