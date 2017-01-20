e4716c9a7951cc077236484fe13429bbf6125e7d
Trevor Joynson (trevorj) <github@skywww.net>
2017-01-19 21:50:24 -0800

    Up

---
+++ bin/zfs-lib.zsh
@@ -0,0 +1,17 @@
+#!/bin/zsh
+
+zfs-exists zpool-exists() {
+	${0%%-*} get -H name $1 #>/dev/null 2>&1
+}
+
+zfs-list zpool-list () {
+		local cmd=(${(s:-:)0} -H)
+
+		local -U props=()
+		zparseopts -D -K -E 'p+:=props'
+		props=(${(s:,:)props#-p})
+		props=($=props)
+
+		$cmd -o "${(j:,:)props}" "$@"
+}
+
