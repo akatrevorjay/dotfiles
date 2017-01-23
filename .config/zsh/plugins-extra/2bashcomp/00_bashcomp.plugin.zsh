3fd795f3888869d0b5a1949d3eb1ddd88547ca76
Trevor Joynson (trevorj) <github@skywww.net>
2017-01-22 23:54:00 -0800

    Wooo

--- .config/zsh/plugins-extra/2bashcomp/00_bashcomp.plugin.zsh
+++ .config/zsh/plugins-extra/2bashcomp/00_bashcomp.plugin.zsh
@@ -1,3 +1,24 @@
 autoload -U +X bashcompinit
 bashcompinit
 
+# bashcomp emulation doesn't include f:_init_completion .
+# This is a very minimal version of that function.
+function _init_completion() {
+    COMPREPLY=()
+    _get_comp_words_by_ref "$@" cur prev words cword
+}
+
+## zshall(1): _bash_completions:
+#
+# This function is used by two widgets, _bash_complete-word and _bash_list-choices.  It exists to provide compatibility with completion bindings in bash.  The last character of the binding  deter‐
+# mines  what  is completed: `!', command names; `$', environment variables; `@', host names; `/', file names; `~' user names.  In bash, the binding preceded by `\e' gives completion, and preceded
+# by `^X' lists options.  As some of these bindings clash with standard zsh bindings, only `\e~' and `^X~' are bound by default.  To add the rest, the following should be  added  to  .zshrc  after
+# compinit has been run:
+for key in '!' '$' '@' '/' '~'; do
+	bindkey "\e$key" _bash_complete-word
+	bindkey "^X$key" _bash_list-choices
+done
+# This includes the bindings for `~' in case they were already bound to something else; the completion code does not override user bindings.
+#
+## zshall(1) _bash_completions$
+
