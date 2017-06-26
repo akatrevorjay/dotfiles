d8b6d4d18481f597c6ea295441489a2c24b534c1
Trevor Joynson (trevorj) <github@skywww.net>
2017-06-25 19:16:54 -0700

    up

--- .config/zsh/plugins/nix/nix.plugin.zsh
+++ .config/zsh/plugins/nix/nix.plugin.zsh
@@ -4,6 +4,8 @@
 : ${NIX_PROFILE_ENV_FILE:=$NIX_PROFILE/etc/profile.d/nix.sh}
 : ${NIX_AUTOLOAD:=true}
 
+autoload -Uz add-suffix-paths bool-is-true
+
 function nix.exists() {
     [[ -e $NIX_PROFILE_ENV_FILE ]]
 }
@@ -12,16 +14,19 @@ function nix.load() {
     # quiet down +warn_create_global
     export NIX_LINK
 
+    add-suffix-paths $NIX_PROFILE
+
     [[ ! -e $NIX_PROFILE_ENV_FILE ]] \
         || source $NIX_PROFILE_ENV_FILE
 
     [ $# -eq 0 ] || ${(@q)argv}
 }
 
-autoload -Uz bool-is-true
 if bool-is-true $NIX_AUTOLOAD; then
     nix.load
+else
+    # Add in completions even if we aren't in the nix env fully. This is also done in add-suffix-paths but that's only on
+    # load.
+    fpath+=($NIX_PROFILE/share/zsh/site-functions)
 fi
 
-# Add in completions
-fpath+=($NIX_PROFILE/share/zsh/site-functions)
