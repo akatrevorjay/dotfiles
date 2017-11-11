347fdc961066b82793f9787ec5c5dbea967253b9
Trevor Joynson (trevorj) <github@skywww.net>
2017-11-11 08:27:45 -0800

    whoops

---
+++ .config/zsh/plugins-trevorj/lolbar/lolbar.plugin.zsh
@@ -0,0 +1,39 @@
+#!/bin/zsh
+
+lolbar() {
+	(( ${+commands[spark]} )) || return
+
+	local cols=$(tput cols)
+	local bar=$(seq 1 $cols | sort -R | spark)
+
+	if (( ${+commands[lolcat]} )); then
+		lolcat <<< $bar
+	else
+		echo $bar
+	fi
+
+	echo; echo
+}
+
+clear() {
+	command clear
+	echo; echo
+	lolbar
+}
+
+lollinit-random-git-tip() {
+	(( ${+commands[git-tip]} )) || return
+	git-tip || :
+	echo
+}
+
+lollinit-random-quote() {
+    (( ${+commands[random-quote]} )) || return
+	random-quote || :
+	echo
+}
+
+lollinit-random-git-tip
+lollinit-random-quote
+lolbar
+
