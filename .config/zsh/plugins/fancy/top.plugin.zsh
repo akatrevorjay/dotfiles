ee35fe6f8395d8a5058f1bdbf8427a946bfd462a
Trevor Joynson (trevorj) <github@skywww.net>
2018-10-14 15:16:43 -0700

    top

---
+++ .config/zsh/plugins/fancy/top.plugin.zsh
@@ -0,0 +1,33 @@
+#!/bin/zsh
+
+set -xv
+
+() {
+	local fn=$HOME/.toprc
+	[[ ! -e $fn ]] || return
+
+	if [ ${commands[top]} -nt $fn -o ! -s $fn ]; then
+		(( ${+commands[top]} )) || return
+		(( ${+commands[dpkg]} )) || return
+
+		local topver=$(
+			dpkg -l procps | tail -n1 | sed -rne 's@^.+\s+procps\s+([^ \s]+)\s+.*$@\1@p'
+		)
+		topver=${topver#*:}
+
+		local major=${topver%%.*}
+		[[ -n $major ]] || return
+
+		local try=($fn-v$major(.-N))
+
+		local src
+		for src in $try; do
+			[[ -e $src ]] || continue
+
+			cp -av $src $fn
+			break
+		done
+	fi
+}
+
+set +xv
